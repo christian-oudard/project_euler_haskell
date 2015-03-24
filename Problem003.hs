abea21156ea280c18c3fc9b90a8148dc9ffdf299
Christian Oudard <christian.oudard@gmail.com>
2015-03-24 10:48:57 -0700

    Refactored problem 3.

--- Problem003.hs
+++ Problem003.hs
@@ -1,17 +1,11 @@
 main = print $ maximum (factorize 600851475143)
 
--- Try dividing n by a list of integers. Return a list whose product is n.
-tryDiv :: Integer -> [Integer] -> [Integer]
-tryDiv n [] = [n]
-tryDiv n (x:xs)
-    | (r == 0) = [x,q]
-    | otherwise = tryDiv n xs
-    where (q,r) = divMod n x
-
 factorize :: Integer -> [Integer]
-factorize n
-    | (length divs == 1) = divs
-    | otherwise = init divs ++ factorize (last divs)
-    where
-        divs = tryDiv n nums
-        nums = 2:[3,5..n `div` 2]
+factorize n = tryDiv n (2:[3,5..])
+
+tryDiv n (x:xs)
+    | x^2 > n    = [n]
+    | r == 0     = x : tryDiv q (x:xs)
+    | otherwise  = tryDiv n xs
+        where
+            (q,r) = divMod n x
